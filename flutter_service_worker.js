062a23a0d540a47900a2fc19db891decd95845d9
Kiarash Alinejad <kiarash.alinejad@gmail.com>
2020-03-07 21:44:22 +0330

    test

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,19 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "3758d562fe64ddb21c103947e2ac1769",
-"/assets\assets\images\failed.png": "5a7f4af0628f04e7ff8ba40cca743a76",
-"/assets\assets\images\foo.png": "239e6c54a21de2b18db1310404c70ee4",
+  "/assets\AssetManifest.json": "6c64bb7d7dfcaa856792ae70b3761611",
 "/assets\FontManifest.json": "f10ea0eb6f6c595664ec1622a9ef347f",
 "/assets\fonts\Dchi.ttf": "40170c03e21221e2bdac2a39ff63d385",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "/assets\fonts\Samim-FD.ttf": "f95b5825d726504459226099198993db",
-"/assets\LICENSE": "86ac3aad54df55cf63b7007c00ffdd7c",
-"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/assets\LICENSE": "4794d8752b82fa5bbad8e9a303596eb4",
+"/icons\Icon-192.png": "5c78e7b20264b35edf850c9390c9cfda",
+"/icons\Icon-512.png": "8a73db98a1d593d4151f32101e4e211f",
 "/index.html": "e5a46cfd15ed76b0ef69f89ee4ca93dc",
-"/main.dart.js": "8bc83534419c76787887db49bd08c8be",
-"/manifest.json": "51eb5e6ba79c810fdeff37192178d0ce"
+"/main.dart.js": "dc31e8b8115c7bd05a166c327c9e2e7f",
+"/manifest.json": "eb03774766e8281437a9e7dadb4e48de"
 };
 
 self.addEventListener('activate', function (event) {
